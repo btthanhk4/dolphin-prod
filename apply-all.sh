9814104bebf8f08c4485bce0760af802b73a9b2a
btthanhk4 <thanhbeo2k4@gmail.com>
2026-01-21 11:09:23 +0700

    update prod 1

--- apply-all.sh
+++ apply-all.sh
@@ -3,38 +3,38 @@
 echo "Applying DolphinScheduler YAML files..."
 
 echo "1. Applying namespace..."
-kubectl apply -f uat-namespace.yaml
+kubectl apply -f prod-namespace.yaml
 
 echo "2. Applying serviceaccount..."
-kubectl apply -f uat-serviceaccount.yaml
+kubectl apply -f prod-serviceaccount.yaml
 
 echo "3. Applying secret..."
-kubectl apply -f uat-secret.yaml
+kubectl apply -f prod-secret.yaml
 
 echo "4. Applying configmaps..."
-kubectl apply -f uat-configmaps.yaml
+kubectl apply -f prod-configmaps.yaml
 
 echo "5. Applying PVCs..."
-kubectl apply -f uat-pvc.yaml
+kubectl apply -f prod-pvc.yaml
 
 echo "6. Applying services..."
-kubectl apply -f uat-services.yaml
+kubectl apply -f prod-services.yaml
 
 echo "7. Applying zookeeper..."
-kubectl apply -f uat-cluster-zookeeper.yaml
+kubectl apply -f prod-cluster-zookeeper.yaml
 
 echo "8. Applying master..."
-kubectl apply -f uat-cluster-master.yaml
+kubectl apply -f prod-cluster-master.yaml
 
 echo "9. Applying worker..."
-kubectl apply -f uat-cluster-worker.yaml
+kubectl apply -f prod-cluster-worker.yaml
 
 echo "10. Applying api..."
-kubectl apply -f uat-cluster-api.yaml
+kubectl apply -f prod-cluster-api.yaml
 
 echo "11. Applying alert..."
-kubectl apply -f uat-cluster-alert.yaml
+kubectl apply -f prod-cluster-alert.yaml
 
 echo "Done! Checking pod status..."
-kubectl get pods -n bnctl-dolphinscheduler-uat-ns
+kubectl get pods -n bnctl-dolphinscheduler-prod-ns
 
